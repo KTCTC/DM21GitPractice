d94491a29ba3cfe7d0f1567c6997a0704cbda2d2
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-05-08 09:00:10 +0530

    updated code

--- src/test/java/abcd/DM21GitPractive/SampleClassForGitPractice.java
+++ src/test/java/abcd/DM21GitPractive/SampleClassForGitPractice.java
@@ -5,6 +5,7 @@ public class SampleClassForGitPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("updated code");
+		System.out.println("second line updated code");
 
 	}
 
---
+++ src/test/java/abcd/DM21GitPractive/WebPage.java
@@ -0,0 +1,11 @@
+package abcd.DM21GitPractive;
+
+public class WebPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample");
+
+	}
+
+}
