619ad1a74a426f6383f3667a8ba280b77bce6f50
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-05-08 08:46:33 +0530

    added sample class and modified App test

---
+++ src/test/java/abcd/DM21GitPractive/SampleClassForGitPractice.java
@@ -0,0 +1,11 @@
+package abcd.DM21GitPractive;
+
+public class SampleClassForGitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("updated code");
+
+	}
+
+}
